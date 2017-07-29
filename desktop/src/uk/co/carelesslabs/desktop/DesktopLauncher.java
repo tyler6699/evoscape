b8535ebeb825b40bf24b3aee7d3605551d3681c4
Ryan Tyler <ryan.tyler@southwales.ac.uk>
2017-07-29 22:26:04 +0100

    Added new LibGDX game
    Updated desktop launcher, set width and hight.

---
+++ desktop/src/uk/co/carelesslabs/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package uk.co.carelesslabs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.carelesslabs.gameclass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new gameclass(), config);
+		config.width = 800;
+		config.height = 600;
+	}
+}
