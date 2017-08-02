8b7dc760302a3b861d54f950f04f52b53f229d28
Ryan Tyler <ryan.tyler@southwales.ac.uk>
2017-08-03 00:50:34 +0100

    Remove new line, white space.

--- core/src/uk/co/carelesslabs/Entity.java
+++ core/src/uk/co/carelesslabs/Entity.java
@@ -1,7 +1,6 @@
 package uk.co.carelesslabs;
 
 import uk.co.carelesslabs.Enums.ENTITYTYPE;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
